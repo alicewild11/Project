28e3c22870c3612b8540b40b483237d443481d09
alicewild <alicewild11@yahoo.co.uk>
2021-03-02 16:44:08 +0000

    Firebase Authentication

--- app/src/main/java/com/example/projectapp/Register_Activity.java
+++ app/src/main/java/com/example/projectapp/Register_Activity.java
@@ -1,14 +1,89 @@
 package com.example.projectapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.HashMap;
 
 public class Register_Activity extends AppCompatActivity {
+
+    MaterialEditText username, email, password;
+    Button btn_register;
+
+    FirebaseAuth auth;
+    //To read or write data from the database, you need an instance of DatabaseReference
+    //declare database reference
+    DatabaseReference database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_);
+
+        //finding the view by the ID stated in xml file
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.emailAddress);
+        password = findViewById(R.id.password);
+        btn_register = findViewById(R.id.btn_register);
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance();
+
+
+
+    }
+
+    private void register(final String username, String email, String password) {
+
+        //Create a new account by passing the new user's email
+        // address and password to createUserWithEmailAndPassword:
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            assert firebaseUser != null;
+                            String userid = firebaseUser.getUid();
+
+                            database = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username);
+                            hashMap.put("imageURL", "default");
+                            hashMap.put("status", "offline");
+                            hashMap.put("search", username.toLowerCase());
+
+                            database.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(Register_Activity.this, Main_Activity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(Register_Activity.this, "You can't Register with this Email or Password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
