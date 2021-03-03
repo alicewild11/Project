beda57366144cf4dcab4e0caefad59178859c313
alicewild <alicewild11@yahoo.co.uk>
2021-03-03 14:42:54 +0000

    Login, Register and Start Activity

--- app/src/main/java/com/example/projectapp/Start_Activity.java
+++ app/src/main/java/com/example/projectapp/Start_Activity.java
@@ -1,14 +1,57 @@
 package com.example.projectapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Start_Activity extends AppCompatActivity {
+
+    Button login, register;
+
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //check if user is null
+        if (firebaseUser != null){
+            Intent intent = new Intent(Start_Activity.this, Main_Activity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
+
+
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Start_Activity.this, Login_Activity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Start_Activity.this, Register_Activity.class));
+            }
+        });
     }
 }
